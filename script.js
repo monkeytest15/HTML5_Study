8cfaf273a331504023da0a06604ee5b594f317f1
Monkey <monkey@MonkeydeMacBook-Pro.local>
2014-11-26 14:25:46 +0800

    js study

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by monkey on 14/11/25.
+ */
+function draw(id){
+    var canvas = document.getElementById(id);
+    if (canvas == null)
+        return false;
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#EEEEFF";
+    context.fillRect(0,0,400,400);
+    context.fillStyle = "red";
+    context.strokeStyle = "blue";
+    context.lineWidth = 1;
+    context.fillRect(50,50,100,100);
+    context.strokeRect(50,50,100,100);
+}
